17d72f9cd8f9a1908f5d33e9fe09c0cb187a81d4
Zak Pie <zakpie@protonmail.com>
2021-04-03 15:07:44 +0000

    update

--- vue.config.js
+++ vue.config.js
@@ -16,46 +16,49 @@ const zopfli = require("@gfx/zopfli");
 
 let plugins = [];
 
-let typeToCompress = /\.(js|css|html|svg|json|png|jpe?g|woff(2?)|ttf|eot|map)$/;
+if(process.env.NODE_ENV == "production"){
+    let typeToCompress = /\.(js|css|html|svg|json|png|jpe?g|woff(2?)|ttf|eot|map)$/;
 
-plugins.push(  new CompressionPlugin({
-    filename: "[path][base].br",
-    algorithm: "brotliCompress",
-    test: typeToCompress,
-    compressionOptions: {
-        params: {
-            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+    plugins.push(  new CompressionPlugin({
+        filename: "[path][base].br",
+        algorithm: "brotliCompress",
+        test: typeToCompress,
+        compressionOptions: {
+            params: {
+                [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+            },
         },
-    },
-    threshold: 0,
-    minRatio: 0.8,
-}))
+        threshold: 0,
+        minRatio: 0.8,
+    }))
 
-plugins.push(new CompressionPlugin({
-    filename: "[path][base].gz",
-    test: typeToCompress,
-    compressionOptions: {
-        numiterations: 15,
-    },
-    algorithm(input, compressionOptions, callback) {
-        return zopfli.gzip(input, compressionOptions, callback);
-    },
-    threshold: 0,
-    minRatio: 0.8,
-}))
-    
-plugins.push( new ImageminPlugin());
-//pluigins.push(_copyPlugin);
+    plugins.push(new CompressionPlugin({
+        filename: "[path][base].gz",
+        test: typeToCompress,
+        compressionOptions: {
+            numiterations: 15,
+        },
+        algorithm(input, compressionOptions, callback) {
+            return zopfli.gzip(input, compressionOptions, callback);
+        },
+        threshold: 0,
+        minRatio: 0.8,
+    }))
+        
+    //plugins.push( new ImageminPlugin());
+    //pluigins.push(_copyPlugin);
 
+    /*
+    plugins.push(
+        new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: [ '/' ],
+        })
+    )*/
 
-plugins.push(
-    new PrerenderSPAPlugin({
-      // Required - The path to the webpack-outputted app to prerender.
-      staticDir: path.join(__dirname, 'dist'),
-      // Required - Routes to render.
-      routes: [ '/' ],
-    })
-)
+} 
 
 module.exports = {
 
