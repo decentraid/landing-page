565333a5be0b159634bc87e18a9215e884096f3b
Zak Pie <zakpie@protonmail.com>
2021-04-03 14:03:02 +0000

    update

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import router from './router'
 import utils from "./mixins/utils"
 import _Globals from "./plugins/_Globals"
 import _I18n from "./plugins/_I18n"
-import "./assets/js/commons"
 import vueSvgImg from '@libertypie/vue-svg-img'
 
 createApp(App)
